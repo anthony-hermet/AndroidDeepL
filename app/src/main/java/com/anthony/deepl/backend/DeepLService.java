e3bfd33197d324265446668f24b52a2087bd4456
Anthony Reboul <anthony.reboul@gmail.com>
2018-02-19 12:44:59 +0100

    Fixed auto-detect language problems - Issue #28

--- app/src/main/java/com/anthony/deepl/backend/DeepLService.java
+++ app/src/main/java/com/anthony/deepl/backend/DeepLService.java
@@ -5,12 +5,14 @@ import com.anthony.deepl.model.TranslationResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface DeepLService {
 
     String BASE_URL = "https://www.deepl.com";
 
+    @Headers("accept-language: en,de,fr,es,it,nl,pl")
     @POST("/jsonrpc")
     Call<TranslationResponse> translateText(@Body TranslationRequest translationRequest);
 
